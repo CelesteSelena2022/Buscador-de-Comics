9b2a55c1951b294065860624a47c777257a52758
CelesteSelena2022 <109759909+CelesteSelena2022@users.noreply.github.com>
2023-10-17 16:27:47 -0300

    fix: elimino el console.log

--- js/api.js
+++ js/api.js
@@ -3,7 +3,6 @@ const getJobs = async () => {
         //showView("spinner")
         let response = await fetch(`https://652753f2917d673fd76d931d.mockapi.io/api/jobs`);
         let data = await response.json();
-        console.log(data);
         renderHome(data)
 
         //setTimeout(() => {
