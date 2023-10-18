b75b695fa72abe549459a638842f65918f3a5943
CelesteSelena2022 <109759909+CelesteSelena2022@users.noreply.github.com>
2023-10-18 15:13:41 -0300

    feat: agrego funciones vistas en clase aun sin modificar

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 const getJobs = async () => {
     try {
-        //showView("spinner")
+        showView("spinner");
         let response = await fetch(`https://652753f2917d673fd76d931d.mockapi.io/api/jobs`);
         let data = await response.json();
         renderHome(data)
@@ -13,11 +13,33 @@ const getJobs = async () => {
         alert("Hubo un error")
         console.log(error);
     }
-}
+};
 
-getJobs();
+const postJob = async (newJob) => {
+    showView("spinner");
+    await fetch(`https://652753f2917d673fd76d931d.mockapi.io/api/jobs`, {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify(newJob),
+    });
+
+    getJobs(); // crear funcion
+    cleanForm(); // crear funcion
+};
+
+// const getJobDetail = async (id) => {
+//     showView("spinner");
+//     let response = await fetch(`https://652753f2917d673fd76d931d.mockapi.io/api/jobs/${id}`
+//     );
+//     let data = await response.json();
+
+//     showJobDetails(data); // crear funcion
+//     editValues(data); // crear funcion
+// };
 
 const renderHome = (data) => {
     renderJobs(data);
-}
-
+    //getShips(data);
+    //getSenority(data);
+    //getDepartments(data);
+};
--- js/main.js
+++ js/main.js
@@ -27,3 +27,49 @@ const renderJobs = (data) => {
             `;
         }
 };
+
+const newJobs = () =>  {
+    let newJobs = {
+        name: "FrontEnd Developer", //value de input
+        image: "https://someimageaboutthejob.com/job1.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Faucibus turpis in eu mi bibendum neque. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Congue nisi vitae suscipit tellus mauris a diam maecenas sed. Sagittis purus sit amet volutpat consequat mauris nunc congue. Sagittis purus sit amet volutpat consequat mauris nunc congue.",
+        location: "Remote",
+        category: "Development",
+        seniority: "Junior",
+        benefits: {
+            "vacation": "3 weeks",
+            "health_ensurance": "OSDE 210",
+            "internet_paid": true
+        },
+        salary: 350000,
+        long_term: true,
+        languages: ["ReactJS", "Javascript", "SASS", "NodeJS"]
+    }; //post para subirlo
+    addJobs(newJobs);
+}
+
+
+//Limpiar el formulario
+
+const cleanForm = () => {
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+    $("").checked = false;
+    $("").value = "";
+    $("").checked = false;
+    $("").value = "";
+    $("").value = "";
+    $("").value = "";
+}
+
+$("#home-btn").addEventListener(`click`, () => getJobs());
+$("#create-job").addEventListener(`click`, () => newJObView());
+$("#submit-job").addEventListener(`click`, () => createNewJob());
+
+window.onload = getJobs();
